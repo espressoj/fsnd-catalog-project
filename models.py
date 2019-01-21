92ae1f550bd4611a59152ceb5e19c3baf98317b3
vagrant <vagrant@vagrant.vm>
2019-01-21 14:29:32 +0000

    Models.py revised to PEP8 standards

--- models.py
+++ models.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env Python
 # Import classes from SqlAlchemy to create the database
-from sqlalchemy import(create_engine, Column, ForeignKey, Integer, String, DateTime,
-                        UniqueConstraint, inspect)
+from sqlalchemy import(create_engine, Column, ForeignKey, Integer, String,
+                       DateTime, UniqueConstraint, inspect)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, joinedload
 # Import class for password and token handling
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer,
-                        BadSignature, SignatureExpired)
+                         BadSignature, SignatureExpired)
 # Import additional libraries needed
 import os
 import sys
-import random, string
+import random
+import string
 
 
 # Construct the base class
 Base = declarative_base()
-secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+secret_key = ''.join(random.choice(string.ascii_uppercase +
+                     string.digits) for x in range(32))
 
 
 # Create a class for the Employee table
@@ -32,42 +34,45 @@ class User(Base):
     email = Column(String)
     password_hash = Column(String(64))
     items = relationship("Items")
+
     @property
     # Set the serialized data pattern for formatting JSON data
     def serialize(self):
         """Return a serialized object for user"""
         return {
-            'id'           : self.id,
-            'name'         : self.name,
-            'username'     : self.username,
-            'picture'      : self.picture,
-            'email'        : self.email,
+            'id': self.id,
+            'name': self.name,
+            'username': self.username,
+            'picture': self.picture,
+            'email': self.email,
         }
 
     # Hash the password
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
+
     # Verfiy the password (hash)
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
+
     # Generate an authorization token that expires in 10 minutes
     def generate_auth_token(self, expiration=600):
-    	s = Serializer(secret_key, expires_in = expiration)
-    	return s.dumps({'id': self.id })
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
 
     @staticmethod
     def verify_auth_token(token):
-    	s = Serializer(secret_key)
-    	try:
-    		data = s.loads(token)
-    	except SignatureExpired:
-    		#Valid Token, but expired
-    		return None
-    	except BadSignature:
-    		#Invalid Token
-    		return None
-    	user_id = data['id']
-    	return user_id
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # Valid Token, but expired
+            return None
+        except BadSignature:
+            # Invalid Token
+            return none
+        user_id = data['id']
+        return user_id
 
 
 # Create a class for the Items table
@@ -76,30 +81,32 @@ class Items(Base):
     # Declare the table name as a reference
     __tablename__ = 'items'
     # Define the columns in the table and their attributes
-    itemId =            Column(Integer, primary_key = True)
-    itemName =          Column(String(100), nullable = False)
-    itemDescription =   Column(String(255), nullable = False)
-    owner =             Column(Integer, ForeignKey('user.id'))
-    added =             Column(DateTime, nullable = False)
-    modified =          Column(DateTime, nullable = False)
-    status =            Column(String(1), nullable = False)
-    # Relationships with other classes and include cascade delete where necessary
-    item_categories = relationship("ItemCategories", cascade="save-update, merge, delete-orphan"
-                                    , single_parent=True)
-    item_inventory = relationship("Inventory", cascade="save-update, merge, delete-orphan"
-                                    , single_parent=True)
-    item_photo = relationship("ItemPhotos", cascade="save-update, merge, delete-orphan"
-                                    , single_parent=True)
-    item_tags = relationship("ItemTags", cascade="save-update, merge, delete-orphan"
-                                    , single_parent=True)
+    itemId = Column(Integer, primary_key=True)
+    itemName = Column(String(100), nullable=False)
+    itemDescription = Column(String(255), nullable=False)
+    owner = Column(Integer, ForeignKey('user.id'))
+    added = Column(DateTime, nullable=False)
+    modified = Column(DateTime, nullable=False)
+    status = Column(String(1), nullable=False)
+    """Relationships with other classes and include cascade delete where
+    necessary"""
+    item_categories = relationship("ItemCategories", cascade="save-update, " +
+                                   "merge, delete-orphan", single_parent=True)
+    item_inventory = relationship("Inventory", cascade="save-update, " +
+                                  "merge, delete-orphan", single_parent=True)
+    item_photo = relationship("ItemPhotos", cascade="save-update, merge, " +
+                              "delete-orphan", single_parent=True)
+    item_tags = relationship("ItemTags", cascade="save-update, merge, " +
+                             "delete-orphan", single_parent=True)
+
     # Set the serialized data pattern for formatting JSON data
     @property
     def serialize(self):
         """Return a serialized object for user"""
         return {
-            'item_id'           : self.itemId,
-            'item_name'         : self.itemName,
-            'item_desc'         : self.itemDescription,
+            'item_id': self.itemId,
+            'item_name': self.itemName,
+            'item_desc': self.itemDescription,
         }
 
 
@@ -109,18 +116,20 @@ class Categories(Base):
     # Declare the table name as a reference
     __tablename__ = 'categories'
     # Define the columns in the table and their attributes
-    categoryId = Column(Integer, primary_key = True)
-    categoryName = Column(String(255), nullable = False)
+    categoryId = Column(Integer, primary_key=True)
+    categoryName = Column(String(255), nullable=False)
     # Relate Items
     itemCategories = relationship("ItemCategories")
+
     # Set the serialized data pattern for formatting JSON data
     @property
     def serialize(self):
         """Return a serialized object for user"""
         return {
-            'category_id'         : self.categoryId,
-            'category_name'       : self.categoryName,
+            'category_id': self.categoryId,
+            'category_name': self.categoryName,
         }
+
 
 # Create a class for the Inventory table
 class Inventory(Base):
@@ -128,20 +137,23 @@ class Inventory(Base):
     # Declare the table name as a reference
     __tablename__ = 'inventory'
     # Define the columns in the table and their attributes
-    itemId = Column(Integer, ForeignKey('items.itemId'), primary_key = True)
-    inventoryCount = Column(Integer, nullable = False)
-    itemPrice = Column(String(25), nullable = True)
-    lastUpdated = Column(DateTime, nullable = False)
+    itemId = Column(Integer, ForeignKey('items.itemId'), primary_key=True)
+    inventoryCount = Column(Integer, nullable=False)
+    itemPrice = Column(String(25), nullable=True)
+    lastUpdated = Column(DateTime, nullable=False)
     # Relationships with other classes
-    # Relate to Items and include cascade delete command and backref to inventory
-    items = relationship("Items", backref='inv', cascade="delete-orphan", single_parent=True)
+    """Relate to Items and include cascade delete command and backref to
+    inventory"""
+    items = relationship("Items", backref='inv', cascade="delete-orphan",
+                         single_parent=True)
+
     # Set the serialized data pattern for formatting JSON data
     @property
     def serialize(self):
         """Return a serialized object for user"""
         return {
-            'inventoryCount'  : self.inventoryCount,
-            'itemPrice'       : self.itemPrice,
+            'inventoryCount': self.inventoryCount,
+            'itemPrice': self.itemPrice,
         }
 
 
@@ -151,9 +163,9 @@ class ItemCategories(Base):
     # Declare the table name as a reference
     __tablename__ = 'itemCategories'
     # Define the columns in the table and their attributes
-    itemCategoryId = Column(Integer, primary_key = True)
+    itemCategoryId = Column(Integer, primary_key=True)
     categoryId = Column(Integer, ForeignKey('categories.categoryId'))
-    itemId = Column(Integer, ForeignKey('items.itemId'), primary_key = True)
+    itemId = Column(Integer, ForeignKey('items.itemId'), primary_key=True)
     # Relationships with Items and include cascade delete
     items = relationship("Items", cascade="delete-orphan", single_parent=True)
 
@@ -164,9 +176,9 @@ class ItemTags(Base):
     # Declare the table name as a reference
     __tablename__ = 'itemTags'
     # Define the columns in the table and their attributes
-    tagId = Column(Integer, primary_key = True)
+    tagId = Column(Integer, primary_key=True)
     itemId = Column(Integer, ForeignKey('items.itemId'))
-    tag = Column(String(80), nullable = False)
+    tag = Column(String(80), nullable=False)
     # Relate to Items and include cascade delete
     items = relationship("Items", cascade="delete-orphan", single_parent=True)
 
@@ -177,17 +189,18 @@ class ItemPhotos(Base):
     # Declare the table name as a reference
     __tablename__ = 'itemPhotos'
     # Define the columns in the table and their attributes
-    photoId = Column(Integer, primary_key = True)
+    photoId = Column(Integer, primary_key=True)
     itemId = Column(Integer, ForeignKey('items.itemId'))
-    photoUrl = Column(String(500), nullable = False)
+    photoUrl = Column(String(500), nullable=False)
     # Relate to Items
     items = relationship(Items)
+
     # Set the serialized data pattern for formatting JSON data
     @property
     def serialize(self):
         """Return a serialized object for user"""
         return {
-            'category_id'   : self.photoUrl,
+            'category_id': self.photoUrl,
         }
 
 # Set the Dialect and Pool using SqlAlchemy's create_engine()
